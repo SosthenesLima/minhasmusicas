bfba5a92bf637c653d87aa357bb195a16a953c14
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-04-19 21:20:14 -0300

    Commit - Modelando nossa aplicação de áudio01

--- src/br/com/sosthenes/minhasmusicas/modelos/Audio.java
+++ src/br/com/sosthenes/minhasmusicas/modelos/Audio.java
@@ -1,10 +1,13 @@
 /*
    By Sósthenes Oliveira Lima
    18/04/2024
-
  */
 
 package br.com.sosthenes.minhasmusicas.modelos;
 
 public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+
 }
