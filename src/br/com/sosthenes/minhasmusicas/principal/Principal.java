393941fab3d0bc8a626850b4a683115414046dbc
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-04-21 10:35:39 -0300

    Commit - Instanciando músicas e podcasts02

--- src/br/com/sosthenes/minhasmusicas/principal/Principal.java
+++ src/br/com/sosthenes/minhasmusicas/principal/Principal.java
@@ -1,4 +1,38 @@
 package br.com.sosthenes.minhasmusicas.principal;
 
+import br.com.sosthenes.minhasmusicas.modelos.Musica;
+import br.com.sosthenes.minhasmusicas.modelos.Podcast;
+
 public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica =  new Musica();
+        minhaMusica.setTitulo("Inferno");
+        minhaMusica.setCantor("AntiDemon");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+            
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+            
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Sósthenes Lima");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+            
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+
+            
+        }
+    }
+
 }
